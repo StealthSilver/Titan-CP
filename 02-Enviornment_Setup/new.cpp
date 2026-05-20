7be678492f4cebdd097924e17492b3ad9548a2ad
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-05-20 20:02:41 +0530

    snippet

---
+++ 02-Enviornment_Setup/new.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+int main() {
+    // Redirecting input and output to files
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n = 1e18;
+    cout << n << endl;
+
+    return 0;
+}
